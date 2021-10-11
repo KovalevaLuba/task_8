877c590f79a65973addf5219b52c7afd908c4e55
KovalevaLuba <lubakovaleva@inbox.ru>
2021-10-11 17:53:12 +0300

    task8

---
+++ src/task8v2.js
@@ -0,0 +1,12 @@
+const a = {
+    "a" : 5,
+    "b" : 2,
+    "2" : 'lo',
+  };
+    
+  let out = '';
+  for (let key in a) {
+    out += 'Ключ - '+ key + ', значение - ' + a[key] + '\n';
+  }
+  console.log(out);
+  
--- src/test.js
+++ src/test.js
@@ -1,25 +1,11 @@
-let arr = [1, 4, 8, 9, 16, 42, 0];
-
-count(arr);
-
-function count(arr) {
-  let oddAmount = 0;
-  let evenAmount = 0;
-  let nullAmount = 0;
-
+const a = {
+  "a" : 5,
+  "b" : 2,
+  "2" : 'lo',
+};
   
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 0) {
-      nullAmount++;
-    } else if (arr[i] %2 === 0 && arr[i] !== 0){
-      evenAmount++;
-    }
-      else {
-      oddAmount++;
-    }
-
-  }
-  console.log("нулей: " + nullAmount);
-  console.log("нечетных : " + oddAmount);
-  console.log("четных : " + evenAmount);
+let out = '';
+for (let key in a) {
+  out += 'Ключ - '+ key + ', значение - ' + a[key] + '\n';
 }
+console.log(out);
